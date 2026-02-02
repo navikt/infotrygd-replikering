1e9209371ef3b69d3fe39e13e829149f97c48dc2
lundasol <Lisa.Marie.Kelly@nav.no>
2026-02-02 20:44:31 +0100

    distroless

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -1,3 +1,15 @@
-export APP_DATASOURCE_URL=$(cat /secrets/oracle/config/jdbc_url)
-export APP_DATASOURCE_USERNAME=$(cat /secrets/oracle/credentials/username)
-export APP_DATASOURCE_PASSWORD=$(cat /secrets/oracle/credentials/password)
+echo "- exporting database username and password -"
+export APP_DATASOURCE_USERNAME=$(cat "$APP_DATASOURCE_USERNAME_PATH" 2> /dev/null || echo $APP_DATASOURCE_USERNAME)
+export APP_DATASOURCE_PASSWORD=$(cat "$APP_DATASOURCE_PASSWORD_PATH" 2> /dev/null || echo $APP_DATASOURCE_PASSWORD)
+echo "- exported APP_DATASOURCE_USERNAME og APP_DATASOURCE_PASSWORD for infotrygd-hentsaksliste-backend "
+
+if [ -z "$APP_DATASOURCE_USERNAME" ]
+then
+  echo "username har ikke verdi"
+fi
+
+
+if [ -z "$APP_DATASOURCE_PASSWORD" ]
+then
+  echo "password har ikke verdi"
+fi
---
+++ src/main/kotlin/no/nav/historisk/innsyn/config/DatasourceConfig.kt
@@ -0,0 +1,66 @@
+package no.nav.historisk.innsyn.config
+
+import jakarta.validation.constraints.NotEmpty
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.validation.annotation.Validated
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.sql.DataSource
+import org.slf4j.LoggerFactory
+
+@Configuration
+@EnableConfigurationProperties(DatasourceConfiguration::class)
+class DatasourceConfig {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Bean
+    fun datasourceConfiguration(): DatasourceConfiguration = DatasourceConfiguration()
+
+    @Bean
+    fun vaultDatasourceUsername(
+        @Value("\${vault.username}") filePath: String,
+    ): String {
+        val path = Paths.get(filePath)
+        logger.debug("Reading datasource username from vault file at path: $filePath")
+        return Files.readString(path)
+    }
+
+    @Bean
+    fun vaultDatasourcePassword(
+        @Value("\${vault.password}") filePath: String,
+    ): String {
+        val path = Paths.get(filePath)
+        logger.debug("Reading datasource password from vault file at path: $filePath")
+        return Files.readString(path)
+    }
+
+    @Bean
+    fun datasource(
+        datasourceConfiguration: DatasourceConfiguration,
+        vaultDatasourceUsername: String,
+        vaultDatasourcePassword: String,
+    ): DataSource {
+        val driverClassName = requireNotNull(datasourceConfiguration.driverClassName) { "spring.datasource.driverClassName is null" }
+        val url = requireNotNull(datasourceConfiguration.url) { "spring.datasource.url is null" }
+        val dataSourceBuilder = DataSourceBuilder.create()
+        dataSourceBuilder.driverClassName(driverClassName)
+        dataSourceBuilder.url(url)
+        dataSourceBuilder.username(vaultDatasourceUsername)
+        dataSourceBuilder.password(vaultDatasourcePassword)
+        return dataSourceBuilder.build()
+    }
+}
+
+@ConfigurationProperties(prefix = "spring.datasource")
+@Validated
+data class DatasourceConfiguration(
+    @NotEmpty
+    var url: String? = null,
+    @NotEmpty
+    var driverClassName: String? = null,
+)
