17b153cf76d2cd1f27c07f534ea204afc15b7a26
Janne <janne.torklep@nav.no>
2026-06-16 12:01:07 +0200

    Fjernet ubrukte dependencies

--- src/test/kotlin/no/nav/historisk/innsyn/ApplicationTest.kt
+++ src/test/kotlin/no/nav/historisk/innsyn/ApplicationTest.kt
@@ -2,11 +2,8 @@ package no.nav.historisk.innsyn
 
 import no.nav.historisk.innsyn.testutil.annotation.IntegrationTest
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.restclient.RestTemplateBuilder
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.getForObject
 
 @IntegrationTest
 class ApplicationTest {
@@ -14,22 +11,12 @@ class ApplicationTest {
     @LocalServerPort
     var port: Int = 0
 
-    @Autowired
-    private lateinit var restTemplateBuilder: RestTemplateBuilder
-
-    private val restTemplate: RestTemplate
-        get() {
-            return restTemplateBuilder
-                .rootUri("http://localhost:$port")
-                .build()
-        }
-
     @Test
     fun contextLoads() {
     }
 
     @Test
     fun health() {
-        restTemplate.getForObject<String>("/actuator/health")
+        RestTemplate().getForObject("http://localhost:$port/actuator/health", String::class.java)
     }
 }
